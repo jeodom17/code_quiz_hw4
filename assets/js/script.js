638b3cc2c7dfce9c59f181c5ef5da67058681b16
mac <jeodom17@gmail.com>
2021-09-20 21:39:35 -0400

    added arrays to .js

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+var quizBox = document.querySelector(".quiz-box");
+var question = document.querySelector(".question");
+var answers = document.querySelector(".answers");
+var answer1 = document.querySelector(".answer1");
+var answer2 = document.querySelector(".answer2");
+var answer3 = document.querySelector(".answer3");
+var answer4 = document.querySelector(".answer4");
+
+// Array for question and answers
+
+var qAndA1 = [
+    {
+        question = "A string can be any text inside which of the following",
+        answer1 = "Parenthesis",
+        answer2 = "Double or single quotes",
+        answer3 = "Brackets",
+        answer4 = "Question marks"
+    }
+]
+var qAndA2 = [
+    {
+        question = "JavaScript booleans can have one of two values:",
+        answer1 = "True or False",
+        answer2 = "HTML or CSS",
+        answer3 = "A number or a letter",
+        answer4 = "A Parent or Child Element"
+    }
+]
+var qAndA3 = [
+    {
+        question = "JavaScript is meant to _:",
+        answer1 = "to define the content of web pages",
+        answer2 = "to specify the layout of web pages",
+        answer3 = "host web pages",
+        answer4 = "to program the behavior of web pages"
+    }
+]
+var qAndA4 = [
+    {
+        question = "The % operator is called a  _:",
+        answer1 = "Increment",
+        answer2 = "Exponentiation",
+        answer3 = "Modulus",
+        answer4 = "Decrement"
+    }
+]
+var qAndA5 = [
+    {
+        question = "x += y is the same as which of the following?",
+        answer1 = "x = y",
+        answer2 = "x + y = z",
+        answer3 = "x = x + y",
+        answer4 = "x + y * y"
+    }
+]
+var qAndA6 = [
+    {
+        question = "Which of the following returns a Random number?",
+        answer1 = "Parenthesis",
+        answer2 = "Math.random();",
+        answer3 = "random.integer",
+        answer4 = "RandomNumber()"
+    }
+]
+
